b56607ad8ba698d6f9eb0cce3eb505df81f9d8e2
arup99-glitch <arupchakraborty153@gmail.com>
2023-07-31 22:36:34 +0600

    Updated

---
+++ src/test/java/com/springBoot/JWT/authentication/and/authorization/JwtAuthenticationAndAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.JWT.authentication.and.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationAndAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
